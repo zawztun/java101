31cfe44705dc0aa2f6ce5e535f09fa18c6e9e0cf
zawztun <zzt.nyc@gmail.com>
2023-10-11 16:04:42 -0400

    created with jpa

--- src/main/java/web/products/ProductController.java
+++ src/main/java/web/products/ProductController.java
@@ -1,25 +1,31 @@
-//package web.products;
-//
-//import java.sql.SQLException;
-//import java.util.concurrent.atomic.AtomicLong;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//import web.utils.records.Product;
-//
-//@RestController
-//public class ProductController {
-//
-//    ProductModal modal = new ProductModal();
-//
-//    public ProductController() throws SQLException {
-//    }
-//
-//    @GetMapping("/product")
-//    public Product getProduct( )  {
-//    modal.getProducts();
-//    return null;
-//    }
-//
-//}
+package web.products;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import web.utils.modals.Products;
+
+@RestController
+public class ProductController {
+  @Autowired private ProjectRepository repository;
+
+  @GetMapping("/products")
+  public @ResponseBody Iterable<Products> getProduct() {
+    Iterable<Products> list = repository.findAll();
+    for (Products product : list) {
+      System.out.println(product);
+    }
+    return repository.findAll();
+  }
+
+  @GetMapping("/add")
+  public @ResponseBody Iterable<Products> add(
+      @RequestParam(value = "title", defaultValue = "World") String title,
+      @RequestParam(value = "price", defaultValue = "2000") int price) {
+    Products newProduct = new Products(title, price);
+    repository.save(newProduct);
+    return repository.findAll();
+  }
+}
